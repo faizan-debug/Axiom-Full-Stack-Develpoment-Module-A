ffcbe2b0df8a4f7042de36ead3dfe2f7ada7da76
Faizan Chaudary <83314898+faizan-debug@users.noreply.github.com>
2024-03-30 01:53:44 +0500

    Updated Project 1 & 2
    Advanced UI for proj 1 & 2 files html, css, js

---
+++ Demo script.js
@@ -0,0 +1,3 @@
+$('#gradient').css({
+    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
