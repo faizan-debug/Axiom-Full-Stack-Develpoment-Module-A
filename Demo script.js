b50ca711faeb70829e678c046ba2bed80a3bc404
Faizan Chaudary <83314898+faizan-debug@users.noreply.github.com>
2024-04-11 22:39:41 +0500

    UI Update
    Nav bar show adjustment

--- Demo script.js
+++
@@ -1,3 +0,0 @@
-$('#gradient').css({
-    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
--- Project6/script.js
+++ Project6/script.js
@@ -9,14 +9,3 @@ const modal = document.getElementById('modal');
 menuToggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
-
-// 2. Listen for click on open button
-open.addEventListener('click', () => modal.classList.add('show-modal') );
-
-// 3. Listen for click on close button
-close.addEventListener('click', () => modal.classList.remove('show-modal') );
-
-// 4. Listen for click outside of modal
-window.addEventListener('click', e => 
-    e.target === modal ? modal.classList.remove('show-modal') : false
-)
